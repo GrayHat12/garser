7fb6493fa50ea54a07b22a2a7e9f924f77a73717
GrayHat12 <grayhathacks10@gmail.com>
2020-03-25 13:03:57 +0530

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -3,8 +3,10 @@
 {
     # command which may fail and give an error 
     scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+    #DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+    #echo("$(DIR)")
 
-    python3 $scriptDir/../pytest.py
+    python3 $scriptDir/pytest.py
 } || {
        # command which should be run instead of the above failing command
        echo "Command exited with an error."
