f3b89b265bdad8cb4d98d3702dcb11aca6a71dbd
GrayHat12 <grayhathacks10@gmail.com>
2020-03-25 17:23:12 +0530

    pypi integration

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'garser',
+    packages = ['garser'],
+    version = 'v1.0-alpha',
+    description = 'An HTML Parser.',
+    author = 'GrayHat',
+    author_email = 'grayhathacks10@gmail.com',
+    url = 'https://github.com/GrayHat12/garser',
+    download_url = 'https://github.com/GrayHat12/garser/archive/v1.0-alpha.tar.gz',
+    keywords = ['Parser', 'Scraping', 'HTMLParser'],
+    classifiers = [],
+)
