5fda746889d3b4fc5ef0ab4b9302d7e96a1972f7
GrayHat12 <grayhathacks10@gmail.com>
2020-03-25 17:54:09 +0530

    first production build

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="garser-grayhat", # Replace with your own username
-    version="1.0",
+    name="garser", # Replace with your own username
+    version="1.1",
     author="GrayHat",
     author_email="garyahthacks10@gmail.com",
     description="A HTML Parser",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/grayhat12/garser",
-    download_url="https://github.com/GrayHat12/garser/archive/v1.0-alpha.tar.gz",
+    download_url="https://github.com/GrayHat12/garser/archive/v1.1.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
+        'License :: OSI Approved :: MIT License',
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
